97ec6651add31b592e5bce72de5dadb30a0eb151
Mohammad Hasan <mohammadhasan707053@gmail.com>
2025-04-24 11:05:54 +0530

    updated

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = 3000
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Signup API. Use POST /signup to register.');
+  });
+  
 app.post('/signup', (req,res) => {
     const{username, email, password, dob} = req.body
 
